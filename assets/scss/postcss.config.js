5f5529a48e00aabdca4da674c077de11226148c4
Paul <64870518+woodcock3@users.noreply.github.com>
2022-07-07 16:35:23 +0100

    Update postcss.config.js

--- assets/scss/postcss.config.js
+++ assets/scss/postcss.config.js
@@ -1,12 +1,19 @@
-module.exports = {
+// postcss.config.js
+
+const autoprefixer = require('autoprefixer');
+const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./**/*.html'],
-  safelist: [],
-  theme: {
-    extend: {
-      colors: {
-        change: 'green',
-      },
-    },
-  },
-  plugins: [require('@tailwindcss/typography')],
-}
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  css: ['city.css'],
+  safelist: {
+    standard: ['::-webkit-scrollbar', '::-webkit-scrollbar-thumb', '::-webkit-scroll-track', 'gradient-text'],
+    greedy: [/^bg-/]
+  }
+});
+
+module.exports = {
+  plugins: [
+    autoprefixer,
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ],
+};
