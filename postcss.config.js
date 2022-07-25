3dfb135ffe01a07a157fee216c01592fde99ee08
Paul <64870518+woodcock3@users.noreply.github.com>
2022-07-25 22:36:53 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ const autoprefixer = require('autoprefixer');
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./**/*.html'],
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-  css: ['city.css'],
+  css: ['style.css'],
   safelist: {
     standard: ['::-webkit-scrollbar', '::-webkit-scrollbar-thumb', '::-webkit-scroll-track', 'gradient-text'],
     greedy: [/^bg-/]
