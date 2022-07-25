5ff634898e07f827bfdae40687148f2fb99161bb
Paul <64870518+woodcock3@users.noreply.github.com>
2022-07-25 23:50:24 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
 // postcss.config.js
 
 const autoprefixer = require('autoprefixer');
+const cssnanoConfig = {
+    autoprefixer: false,
+    discardComments: {removeAll: true},
+    svgo: true
+};
+const cssnano = require('cssnano')({
+  preset: ['default', { cssnanoConfig }]
+});
+
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./**/*.html'],
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
@@ -14,6 +23,6 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 module.exports = {
   plugins: [
     autoprefixer,
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    ...(process.env.NODE_ENV === "production" ? [cssnano, purgecss] : [])
   ],
 };
