3058d051d06d2c0065b8d8d5675400729f4c4c2b
Rustie <64870518+woodcox@users.noreply.github.com>
2023-04-15 17:13:10 +0100

    Update all-img.js

--- config/build/all-img.js
+++ config/build/all-img.js
@@ -2,16 +2,13 @@ const Image = require('@11ty/eleventy-img');
 const glob = require('glob-all');
 const path = require("path");
 
-const allImages = async (
-  widths = [400, 600, 800, 1000, 1160],
-  formats = ['webp', 'jpeg']
-) => {
+const allImages = async () => {
 
   // Use original file slug to write images to dist
   const imagePath = glob.sync(['assets/images/master/*.jpg']);
   await Image(imagePath, {
-    widths: [...widths],
-    formats: [...formats],
+    widths: [400, 600, 800, 1000, 1160],
+    formats: ['webp', 'jpeg'],
     outputDir: 'dist/assets/images',
     urlPath: '/city11/assets/images',
     filenameFormat: function (id, src, width, format, options) {
