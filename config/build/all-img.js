acfada845be9a5787f374656e4a4f2d2373dc66a
Rustie <64870518+woodcox@users.noreply.github.com>
2023-04-15 16:57:21 +0100

    Update all-img.js

--- config/build/all-img.js
+++ config/build/all-img.js
@@ -3,20 +3,20 @@ const glob = require('glob-all');
 const path = require("path");
 
 const allImages = async (
-  url = glob.sync(['assets/images/master/*.jpg']),
+  const imagePath = glob.sync(['assets/images/master/*.jpg']),
   widths = [400, 600, 800, 1000, 1160],
   formats = ['webp', 'jpeg']
 ) => {
 
   // Use original file slug to write images to dist
-  const imageMetadata = await Image(url, {
+  const imageMetadata = await Image(imagePath, {
     widths: [...widths],
     formats: [...formats],
     outputDir: 'dist/assets/images',
     urlPath: '/city11/assets/images',
     filenameFormat: function (id, src, width, format, options) {
-		  const extension = path.extname(src);
-		  const name = path.basename(src, extension);
+      const extension = path.extname(src);
+      const name = path.basename(src, extension);
       
       // writes all images to 'dist/assets/images' with formats of imageFileName-300w.webp and .jpg
       return `${name}-${width}w.${format}`;
