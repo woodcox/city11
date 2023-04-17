c43ae666d95fb3781fe0147b42da8f8785aff92f
Rustie <64870518+woodcox@users.noreply.github.com>
2023-04-17 08:33:15 +0100

    Update and rename site.webmanifest to site.webmanifest.11ty.js

---
+++ src/site.webmanifest.11ty.js
@@ -0,0 +1,28 @@
+---
+layout: null
+---
+
+module.exports = async function() {
+  return await this.renderTemplate(`
+  {
+    "name":"{{ settings.name }}",
+    "short_name":"{{ settings.shortname }}",
+    "icons":[
+      {
+        "src":"/android-chrome-192x192.png",
+        "sizes":"192x192",
+        "type":"image/png"
+      },
+      {
+        "src":"/android-chrome-512x512.png",
+        "sizes":"512x512",
+        "type":"image/png"
+      }
+    ],
+    "theme_color":"{{ settings.primary_color }}",
+    "background_color":"{{ settings.secondary_color }}",
+    "display":"standalone"
+  }`, "liquid");
+};
+
+
