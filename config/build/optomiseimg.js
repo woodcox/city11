ea8f953c4a563fb6b476b07fce51f0fa3cb77682
Rustie <64870518+woodcox@users.noreply.github.com>
2023-04-17 06:41:06 +0100

    Update and rename all-img.js to optomiseimg.js

--- config/build/optomiseimg.js
+++ config/build/optomiseimg.js
@@ -1,9 +1,10 @@
+// optomise all images in the 'assets/images/master' folder
+
 const Image = require('@11ty/eleventy-img');
 const glob = require('glob-all');
 const path = require("path");
 
 const allImages = async () => {
-
   // Use original file slug to write images to dist
   const imagePaths = glob.sync(['assets/images/master/*.jpg']);
 
@@ -14,15 +15,17 @@ const allImages = async () => {
       formats: ['webp', 'jpeg'],
       outputDir: 'dist/assets/images',
       urlPath: '/city11/assets/images',
+      
+      // modify the output file format
       filenameFormat: function(id, src, width, format, options) {
         const { name } = path.parse(src);
         console.log(name);
       
-        // writes all images to 'dist/assets/images' with formats of imageFileName-300w.webp and .jpg
+        // writes all images to 'dist/assets/images' with formats of imageFileName-300.webp and .jpeg
         return `${name}-${width}.${format}`;
       }
     });
   }
 };
 
-module.exports =  allImages;
+module.exports =  optomiseImages;
